635aefbd9841efceb7659a49e657ff1cb1e30d72
omar-hammami <omar.hammami.1992@gmail.com>
2024-05-12 21:06:03 +0200

    Outdated TLS policy

--- listeners.tf
+++ listeners.tf
@@ -2,7 +2,7 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.main.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
   certificate_arn   = aws_acm_certificate_validation.shared.certificate_arn
 
   default_action {
