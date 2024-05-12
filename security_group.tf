0fa0a809adb49b3cf5d3ba4024ac6d1520c6837f
omar-hammami <omar.hammami.1992@gmail.com>
2024-05-12 22:06:03 +0200

    Description in security group rules

--- security_group.tf
+++ security_group.tf
@@ -9,6 +9,7 @@ resource "aws_security_group" "alb" {
     protocol         = "-1"
     cidr_blocks = ["192.168.100.0/24"]  // Example of a more restrictive range
     //ipv6_cidr_blocks = ["2600:1f18:1234::/56"]  // Example of a more restrictive IPv6 range
+    description      = "Permit all outbound traffic to internal network segments"
   }
 
   tags = {
